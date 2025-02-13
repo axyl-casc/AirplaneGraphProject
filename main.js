009e04e07641a5785fb161b85b3e5908459fa2d5
Depanshu <122829642+Suya678@users.noreply.github.com>
2025-02-13 12:04:09 -0700

    Added sample input files

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const loadFiles = require('./fileLoader.js');
 
 const [distanceMatrix, packageData, constraints] = loadFiles();
+console.log(distanceMatrix);
+console.log(packageData);
+console.log(constraints);
 
 
