724bc90d09d8b1444dab25307b2d1346b7eaa300
Kevinmru737 <78282516+Kevinmru737@users.noreply.github.com>
2025-02-14 12:42:44 -0700

    added package.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { parseAndLoadInputFiles } = require("./parser.js");
-
+// node main.js ./sample_input_files/sample_distance.js ./sample_input_files/sample_package.js ./sample_input_files/sample_constraints.js
 const [distanceMatrix, packageData, constraints] = parseAndLoadInputFiles();
 console.log(distanceMatrix);
 console.log(packageData);
